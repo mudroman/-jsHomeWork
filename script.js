dabb2c1b7c9e7ba02702680a8f926ba79db5000f
Roman Mud <roman.mud@globallogic.com>
2021-11-01 21:48:23 +0200

    secind commit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-consolo.log('Hello world');
+consolo.log('Hello world'); 
+
+let myName = 'Roman';
+let myAge = 29
+let obj = {
+    familystatus: 'meried',
+    familymember: 'husban',
+    familyquntity: 1,
+    
+}
