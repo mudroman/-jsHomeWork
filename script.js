666cf0f477fc36a62b8bf57d48ffd11c8d557e18
mudroman <63589730+mudroman@users.noreply.github.com>
2021-11-01 22:23:17 +0200

    Third commit

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 consolo.log('Hello world'); 
 
-let myName = 'Roman';
-let myAge = 29
+let firstName = 'Roman';
+let myAge = 29;
 let obj = {
     familystatus: 'meried',
     familymember: 'husban',
-    familyquntity: 1,
-    
-}
-
-
-
-  'add some commit'
+    familyquntity: 1,   
+};
+  let actions = 'add some commit';x
+  let arr = [1,2,3,4,5,6,7,8,9,10]
+  for(let i = 0; i < 10; i++){
+      console.log(i);
+  };
+Git commit 3
