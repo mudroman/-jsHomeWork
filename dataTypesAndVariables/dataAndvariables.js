8dcdae045179b2d16f2c6033b679ec54d15fb668
mudroman <63589730+mudroman@users.noreply.github.com>
2021-11-05 13:41:21 +0200

    1 lesson

---
+++ dataTypesAndVariables/dataAndvariables.js
@@ -0,0 +1,30 @@
+//primitive
+
+const one = 1;
+const ONE = '1';
+const name = 'Ivan';
+
+
+let varShouldBeUpdated = null;
+
+const a = 'a';
+
+const A = 'A';
+
+let temp = undefined;
+
+// objects
+
+const obj = {
+a: 1,
+name: 'Test',
+};
+
+const ar = [];
+
+console.log(one);
+console.log('It is name variable ', one);
+const res = one === ONE;
+const res2 = one === ONE;
+console.log('Result ', res);
+console.log('Result with data types check ', res2);
