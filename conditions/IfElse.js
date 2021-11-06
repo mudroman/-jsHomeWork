63c540ac21262d8faea17c095ce1570378898b41
mudroman <63589730+mudroman@users.noreply.github.com>
2021-11-06 22:56:11 +0200

    Ready

--- conditions/IfElse.js
+++ conditions/IfElse.js
@@ -1,16 +1,16 @@
-// let mounthNum =  Math.ceil(Math.random() * 12);
+let mounthNum =  Math.ceil(Math.random() * 12);
 
-// console.log(mounthNum);
+console.log(mounthNum);
 
-// if (mounthNum === 1 || mounthNum === 2 || mounthNum === 12) {
-//     console.log( 'Winter is here!');
-// } else if (mounthNum === 3 || mounthNum === 4 || mounthNum === 5 ) {
-//     console.log( 'Spring is here!');
-// } else if (mounthNum === 6 || mounthNum === 7 || mounthNum === 8) {
-//     console.log( 'Summer is here!');
-// } else if (mounthNum === 9 || mounthNum === 10 || mounthNum === 11 ) {
-//     console.log( 'Autumn is here!');
-// }
+if (mounthNum === 1 || mounthNum === 2 || mounthNum === 12) {
+    console.log( 'Winter is here!');
+} else if (mounthNum === 3 || mounthNum === 4 || mounthNum === 5 ) {
+    console.log( 'Spring is here!');
+} else if (mounthNum === 6 || mounthNum === 7 || mounthNum === 8) {
+    console.log( 'Summer is here!');
+} else if (mounthNum === 9 || mounthNum === 10 || mounthNum === 11 ) {
+    console.log( 'Autumn is here!');
+}
 
 
 let i = Math.ceil(Math.random() * 100);
