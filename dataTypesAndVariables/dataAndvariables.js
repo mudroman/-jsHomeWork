c761bfd812e862ed52278226dc9010049a1c752a
mudroman <63589730+mudroman@users.noreply.github.com>
2021-11-06 13:53:38 +0200

    2nd comitt

--- dataTypesAndVariables/dataAndvariables.js
+++ dataTypesAndVariables/dataAndvariables.js
@@ -1,30 +1,51 @@
 //primitive
 
-const one = 1;
-const ONE = '1';
-const name = 'Ivan';
+// const one = 1;
+// const ONE = '1';
+// const name = 'Ivan';
+// let varShouldBeUpdated = null;
+// const a = 'a';
+// const A = 'A';
+// let temp = undefined;
+// // objects
+// const obj = {
+// a: 1,
+// name: 'Test',
+// };
+// const ar = [];
+// console.log(one);
+// console.log('It is name variable ', one);
+// const res = one === ONE;
+// const res2 = one === ONE;
+// console.log('Result ', res);
+// console.log('Result with data types check ', res2);
+//----------------------------------- Home work here---------------------------------------------------------------
+
+const firstName = 'Roman';
+const secondName = 'Mud';
+let age = 29;
+let myAge = 29;
+let familyQuantity;
+const isMarried = true;
+let not = null;
+const snake_case = undefined;
+familyQuantity = 3;
+
+const correctAge = age === myAge ? console.log('Im getting old ):') : console.log('Is it true?');
+const type_OF = firstName === secondName;
+const type_IF = firstName === secondName ? console.log('true') : console.log('It is : ', 'false');
+let verf = age == familyQuantity ? console.log('true') : console.log('it is false');
+
+console.log(type_OF);
 
 
-let varShouldBeUpdated = null;
-
-const a = 'a';
-
-const A = 'A';
-
-let temp = undefined;
-
-// objects
-
-const obj = {
-a: 1,
-name: 'Test',
-};
-
-const ar = [];
-
-console.log(one);
-console.log('It is name variable ', one);
-const res = one === ONE;
-const res2 = one === ONE;
-console.log('Result ', res);
-console.log('Result with data types check ', res2);
+console.log(secondName.length);
+console.log(firstName.toUpperCase());
+console.log(typeof familyQuantity);
+console.log(typeof firstName);
+console.log(typeof secondName);
+console.log(age.toString());
+console.log(familyQuantity);
+console.log(typeof isMarried);
+console.log(typeof snake_case);
+console.log(typeof nothing);
