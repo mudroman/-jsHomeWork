108940ba21eff13d5f737d26792243ac9320d305
mudroman <63589730+mudroman@users.noreply.github.com>
2021-11-09 16:45:17 +0200

    ready to check

---
+++ Odjects/Objects.js
@@ -0,0 +1,107 @@
+
+const myCar = {
+    brand : 'VW',
+    model : 'Passat',
+    years : 15,
+    odometer : '350 000 km',
+    lasrService: '16.02.20',
+};
+
+if (myCar.years <= 10) {
+    myCar.price = 5000;
+} else {
+    myCar.pirce = 2500;
+};
+
+
+console.log(myCar);
+console.log(myCar.hasOwnProperty(myCar.brand));
+
+const person = {
+    firstName : 'Roman',
+    secondName : 'Mud',
+    age : 29,
+    sex : 'male',
+    isMarried : true,
+};
+
+console.log(person.hasOwnProperty('age'));
+
+const myDog = {
+
+    name : 'Jorik',
+    age : 7,
+    size : 'small',
+    color : 'black',
+    speak : () => console.log("Woof! Woof!"),
+};
+
+
+const newDog = Object.assign({}, myDog);
+
+console.log(newDog);
+
+const laptop = {
+    prodName : 'HP',
+    color : 'Grey',
+    core : 'i5',
+    ssd : 512,
+    ram : 8
+};
+
+console.log(laptop.core);
+Object.freeze(laptop);
+laptop.speed = 'fast';
+console.log(laptop);
+
+const myDisplay = {
+    displayName : 'Samsung',
+    color : 'black',
+    size : 24,
+    hasHDMI : true,
+    hasRemote : true,
+}
+
+myDisplay.buttons = 'sensor';
+console.log(Object.keys(myDisplay));
+
+const myHouse = {
+    rooms : 3,
+    haskitchen : true,
+    hasGarage : true,
+};
+
+const myPhone =  {
+    brand : 'Xiaomi',
+    model : 'Mi 8 liet',
+    color : 'Grey',
+    ram : 6,
+    memory : 64,
+};
+
+const  englishCourse = {
+nameOfcourse : 'En lng',
+sessions : 'everyday',
+studensQuantity : 10,
+teachers : 2,
+price : 10000
+
+};
+
+const train = {
+    direction : 'Odessa',
+    ticketPrice : 500,
+    capacity : 500,
+    tripDuration : 12,
+
+};
+
+const cocacola = {
+    price : 29.40,
+    size : 2.25,
+    sugarQuantity : 5 * 0.27,
+    calories : 5 * 105
+};
+
+
+console.log();
